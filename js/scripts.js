6b004674ca9bae469259ba113981275b0dc61613
Quinten Clause <quintenclause@hotmail.com>
2015-12-01 13:27:21 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,14 @@ $(document).ready(function(){
     if (location.hash != '') {
         $('.page').removeClass('active');
         $(location.hash).addClass('active');
+        $('nav a').removeClass('active');
+        $('nav a[href="' + location.hash + '"]').addClass('active');
     }
 
     $('nav a').click(function (e) {
         $('.page').removeClass('active');
         $($(this).attr('href')).addClass('active');
+        $('nav a').removeClass('active');
+        $(this).addClass('active');
     });
 });
